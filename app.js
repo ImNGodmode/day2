72e41788156e0df5a9939e638acf081356e658f9
ImNGodmode <bjames.56@icloud.com>
2022-08-24 22:22:15 -0400

    built the function for save the world btn

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const hulkSnap = document.querySelector('.hulkSnap')
+const hulkSlam = document.querySelector('.hulkSlam')
+
+const saveTheWorldF = () => {
+    hulkSnap.classList.toggle('hulkSnap1');
+}
