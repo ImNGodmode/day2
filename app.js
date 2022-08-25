e62e8a3bfa1776c7583b5f632ce3e899a4d1e2ef
ImNGodmode <bjames.56@icloud.com>
2022-08-24 22:23:40 -0400

    built the function for smash btn

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const hulkSlam = document.querySelector('.hulkSlam')
 const saveTheWorldF = () => {
     hulkSnap.classList.toggle('hulkSnap1');
 }
+const smashF = () => {
+    hulkSlam.classList.toggle('hulkSlam1');
+}
